28ad7cffa3071b06bf719c99e87d093aad845d49
DY_XiaoDong <xiaodong@indouyin.cn>
2022-11-18 07:03:42 +0800

    Update command.je

---
+++ command.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+const program = require('commander');
+
+program
+  .version(`fastjs-cli ${require('package.json').version}`)
+  .usage('<command> [options]');
+
+program
+  .command('create <app-name>')
+  .description('create a new project powered by fastjs-cli')
+  .option('-f, --force', 'overwrite target directory if it exists')
+  .action((name, cmd) => {
+    require('create')(name, cleanArgs(cmd));
+  })
