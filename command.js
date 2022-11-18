118285aebd9fcc1ecdacd297a41ed2eb05629a91
DY_XiaoDong <xiaodong@indouyin.cn>
2022-11-18 19:09:52 +0800

    Update command.js

--- command.js
+++ command.js
@@ -3,14 +3,46 @@
 const program = require('commander');
 
 program
-  .version(`fastjs-cli ${require('package.json').version}`)
+  .name('fastjs')
+  .description('Fastjs cli can help you build a npm project easily.')
   .usage('<command> [options]');
 
 program
-  .command('create <app-name>')
+  .command('create')
+  .arguments('<name>')
   .description('create a new project powered by fastjs-cli')
   .option('-f, --force', 'overwrite target directory if it exists')
   .action((name, cmd) => {
+    // log
+    console.log('test');
     require('lib/create.js')(name, cleanArgs(cmd));
   })
 
+// if no arguments, output help
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
+
+// if arguments name is not in the list, output error
+program.on('command:*', function (operands) {
+  console.log('\x1b[31m' +
+    'Invalid command: ' + operands[0] +
+    '\n' +
+    '\nSee --help for a list of available commands.' +
+    '\x1b[0m');
+})
+
+// [MIT] from https://github.com/vuejs/vue-cli/
+function cleanArgs(cmd) {
+  // setup args
+  const args = {}
+  cmd.options.forEach(arg => {
+    const key = arg.long
+      .replace(/^--/, '')
+      .replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
