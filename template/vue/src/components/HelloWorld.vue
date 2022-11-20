bd72e9b5f2ec91ce8682b454f18de792a5697bfc
DY_XiaoDong <xiaodong@indouyin.cn>
2022-11-20 20:01:57 +0800

    Update template

---
+++ template/vue/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      count: 0,
+    };
+  },
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+  <div class="select">
+    <span>Fastjs&nbsp;|</span>
+    <a href="https://docs.fastjs.com.cn/" target="_blank">Docs</a>
+    <a href="https://github.com/fastjs-team/fastjs-next/" target="_blank">Github</a>
+    <a href="https://fastjs.com.cn/" target="_blank">Website</a>
+  </div>
+  <div class="select">
+    <span>Cli&nbsp;|</span>
+    <a href="https://github.com/fastjs-team/fastjs-cli/" target="_blank">Github</a>
+  </div>
+  <div class="select">
+    <span>Typescript&nbsp;|</span>
+    <a href="https://github.com/topics/typescript" target="_blank">Github</a>
+    <a href="https://www.typescriptlang.org/" target="_blank">Website</a>
+  </div>
+</template>
