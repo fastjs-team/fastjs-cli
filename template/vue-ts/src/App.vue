390f38814160ea869380f5aeb1aa746593ea0945
DY_XiaoDong <xiaodong@indouyin.cn>
2022-11-20 20:24:21 +0800

    Update template

---
+++ template/vue-ts/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <HelloWorld msg="Fastjs + Vue" />
+</template>
+
+<script setup>
+// Vite script setup -> https://vitejs.dev/guide/features.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+</style>
