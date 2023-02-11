472ef307cd2364619225b48604a289383279741b
DY_XiaoDong <xiaodong@indouyin.cn>
2023-02-11 11:07:57 +0800

    Update modules - vue-router

--- modules/vue-router/App.vue
+++ modules/vue-router/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{ Component }">
-    <transition name="fade">
+    <transition name="fade" mode="out-in">
       <component :is="Component" />
     </transition>
   </router-view>
